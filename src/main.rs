a7026d4efdfd3f9664c2b0d5f3296e1e434785a7
cutsea110 <cutsea110@gmail.com>
2022-05-21 08:12:02 +0900

    diamond call

--- src/main.rs
+++ src/main.rs
@@ -124,6 +124,9 @@ impl diamond_capnp::qux::Server for QuxImpl {
     }
 }
 
-fn main() {
-    println!("Hello")
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello");
+
+    Ok(())
 }
