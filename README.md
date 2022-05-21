64d1367f747fedce3496174a60fcfa87af72eb22
cutsea110 <cutsea110@gmail.com>
2022-05-21 11:38:17 +0900

    confirm networking

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # study capnp
+
+## try!
+
+term 0
+
+``` shell
+sudo tcpdump -i lo
+```
+
+term 1
+
+``` shell
+RUST_LOG=trace cargo run -- server
+```
+
+term 2
+
+``` shell
+cargo run -- client
+```
--- src/client.rs
+++ src/client.rs
@@ -2,7 +2,11 @@ use capnp::capability::Promise;
 use capnp_rpc::{pry, rpc_twoparty_capnp, twoparty, RpcSystem};
 use futures::{AsyncReadExt, FutureExt};
 use log::trace;
-use std::net::{SocketAddr, ToSocketAddrs};
+use std::{
+    net::{SocketAddr, ToSocketAddrs},
+    thread,
+    time::Duration,
+};
 
 use crate::diamond_capnp;
 
@@ -77,30 +81,37 @@ async fn try_main(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
 
         let mut bar_req = foo.get_bar_request();
         bar_req.get().set_name("Alice".into());
-        let name = bar_req
-            .send()
-            .pipeline
-            .get_bar()
-            .read_val_request()
-            .send()
-            .promise
-            .await?
-            .get()?
-            .get_val()?
-            .to_string();
+        println!("NET!");
+        thread::sleep(Duration::from_secs(1));
+        let reply = bar_req.send().promise.await?;
+        println!("NET!");
+        thread::sleep(Duration::from_secs(1));
+        let bar = reply.get()?.get_bar()?;
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
+        let reply = bar.read_val_request().send().promise.await?;
+        println!("NET!");
+        thread::sleep(Duration::from_secs(1));
+        let name = reply.get()?.get_val()?.to_string();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
 
         let mut baz_req = foo.get_baz_request();
         baz_req.get().set_age(14);
-        let age = baz_req
-            .send()
-            .pipeline
-            .get_baz()
-            .read_val_request()
-            .send()
-            .promise
-            .await?
-            .get()?
-            .get_val();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
+        let reply = baz_req.send().promise.await?;
+        println!("NET!");
+        thread::sleep(Duration::from_secs(1));
+        let baz = reply.get()?.get_baz()?;
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
+        let reply = baz.read_val_request().send().promise.await?;
+        println!("NET!");
+        thread::sleep(Duration::from_secs(1));
+        let age = reply.get()?.get_val();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
 
         println!("name: {}, age: {}", name, age);
     }
@@ -109,19 +120,39 @@ async fn try_main(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
         println!("second test");
         let mut bar_req = foo.get_bar_request();
         bar_req.get().set_name("Alice".into());
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
         let bar_client = bar_req.send().pipeline.get_bar();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
 
         let mut baz_req = foo.get_baz_request();
         baz_req.get().set_age(14);
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
         let baz_client = baz_req.send().pipeline.get_baz();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
 
         let qux_client: diamond_capnp::qux::Client = capnp_rpc::new_client(QuxImpl::new());
         let mut qux_req = qux_client.calc_request();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
         qux_req.get().set_bar(bar_client);
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
         qux_req.get().set_baz(baz_client);
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
         let reply = qux_req.send().promise.await?;
+        println!("NET!");
+        thread::sleep(Duration::from_secs(1));
         let name = reply.get()?.get_name()?;
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
         let age = reply.get()?.get_age();
+        println!("---");
+        thread::sleep(Duration::from_secs(1));
 
         println!("name: {}, age: {}", name, age);
     }
